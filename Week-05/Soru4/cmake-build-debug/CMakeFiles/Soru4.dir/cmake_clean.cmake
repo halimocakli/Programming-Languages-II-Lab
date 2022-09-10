dddc31386f35ccae6db61627a27594fd8cd4083e
Halim Can Ocaklı <halimcanocakli@gmail.com>
2022-09-10 10:47:05 +0300

    Create cmake_clean.cmake

---
+++ Week-05/Soru4/cmake-build-debug/CMakeFiles/Soru4.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Soru4.dir/main.c.obj"
+  "Soru4.exe"
+  "Soru4.exe.manifest"
+  "Soru4.pdb"
+  "libSoru4.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Soru4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
